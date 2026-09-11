5bb748cb33438f54eb28ee2806abda2a967c631d
PRASHANT KUMAR SAH <zzpscode@gmail.com>
2026-09-12 00:12:03 +0530

    feat: expose persistent search adapter

--- global-automation/ocr/__init__.py
+++ global-automation/ocr/__init__.py
@@ -22,6 +22,7 @@ from .cross_document import CrossDocumentRelation, CrossDocumentGraph, build_cro
 from .document_clustering import CandidateCluster, CandidateClusterResult, build_candidate_clusters, candidate_clusters_to_dict
 from .search_index import SearchDocument, SearchEntity, SearchHit, DocumentSearchIndex, build_search_index, normalize_query, search_to_dict
 from .reference_timeline import TimelineEvent, ReferenceTimeline, build_reference_timeline, reference_timeline_to_dict
+from .persistent_search import SQLiteSearchStore
 
 __all__ = [
     "extract_document_text", "extract_embedded_pdf_text", "ocr_image", "ocr_image_detailed", "render_pdf",
@@ -37,5 +38,5 @@ __all__ = [
     "CrossDocumentRelation", "CrossDocumentGraph", "build_cross_document_graph", "cross_document_graph_to_dict",
     "CandidateCluster", "CandidateClusterResult", "build_candidate_clusters", "candidate_clusters_to_dict",
     "SearchDocument", "SearchEntity", "SearchHit", "DocumentSearchIndex", "build_search_index", "normalize_query", "search_to_dict",
-    "TimelineEvent", "ReferenceTimeline", "build_reference_timeline", "reference_timeline_to_dict",
+    "TimelineEvent", "ReferenceTimeline", "build_reference_timeline", "reference_timeline_to_dict", "SQLiteSearchStore",
 ]
