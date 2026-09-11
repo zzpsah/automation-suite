3153126b19c9d61fbd08c0fb36507bdf505caf4f
PRASHANT KUMAR SAH <zzpscode@gmail.com>
2026-09-11 22:28:11 +0530

    feat(ocr): export structured OCR geometry API

--- global-automation/ocr/__init__.py
+++ global-automation/ocr/__init__.py
@@ -5,6 +5,7 @@ from .ocr_service import process_file, process_image, process_pdf
 from .sarkari_normalizer import SarkariMetadata, extract_metadata, normalize_sarkari_text
 from .backend_policy import choose_backend, choose_from_benchmarks
 from .field_confidence import FieldConfidence, score_field, score_fields
+from .region_alignment import OCRSpan, align_regions
 
 __all__ = [
     "extract_document_text",
@@ -23,4 +24,6 @@ __all__ = [
     "FieldConfidence",
     "score_field",
     "score_fields",
+    "OCRSpan",
+    "align_regions",
 ]
