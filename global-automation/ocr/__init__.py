f4b71cb44df76605e52be76a6331c74090c3ad2b
PRASHANT KUMAR SAH <zzpscode@gmail.com>
2026-09-11 22:32:00 +0530

    feat(ocr): expose spatial layout API

--- global-automation/ocr/__init__.py
+++ global-automation/ocr/__init__.py
@@ -5,6 +5,8 @@ from .ocr_service import process_file, process_image, process_pdf
 from .sarkari_normalizer import SarkariMetadata, extract_metadata, normalize_sarkari_text
 from .backend_policy import choose_backend, choose_from_benchmarks
 from .field_confidence import FieldConfidence, score_field, score_fields
+from .region_alignment import OCRSpan, align_regions
+from .spatial_layout import LayoutRegion, classify_layout
 
 __all__ = [
     "extract_document_text",
@@ -23,4 +25,8 @@ __all__ = [
     "FieldConfidence",
     "score_field",
     "score_fields",
+    "OCRSpan",
+    "align_regions",
+    "LayoutRegion",
+    "classify_layout",
 ]
