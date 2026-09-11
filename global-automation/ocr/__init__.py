46a04c201fa1bf1dd1739ee29db26b9d274473d1
PRASHANT KUMAR SAH <zzpscode@gmail.com>
2026-09-11 20:00:01 +0530

    Expose complete reusable OCR APIs

--- global-automation/ocr/__init__.py
+++ global-automation/ocr/__init__.py
@@ -1,13 +1,18 @@
 """Global Sarkari OCR package."""
 
-from .ocr_engine import extract_document_text, extract_embedded_pdf_text, ocr_image, render_pdf
+from .ocr_engine import extract_document_text, extract_embedded_pdf_text, ocr_image, ocr_image_detailed, render_pdf
+from .ocr_service import process_file, process_image, process_pdf
 from .sarkari_normalizer import SarkariMetadata, extract_metadata, normalize_sarkari_text
 
 __all__ = [
     "extract_document_text",
     "extract_embedded_pdf_text",
     "ocr_image",
+    "ocr_image_detailed",
     "render_pdf",
+    "process_file",
+    "process_image",
+    "process_pdf",
     "SarkariMetadata",
     "extract_metadata",
     "normalize_sarkari_text",
