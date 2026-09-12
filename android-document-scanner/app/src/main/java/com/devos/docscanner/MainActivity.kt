d570775c0a07278ab0380246f3728c94caf85998
PRASHANT KUMAR SAH <zzpscode@gmail.com>
2026-09-13 01:08:10 +0530

    fix(scanner): import Compose setContent

--- android-document-scanner/app/src/main/java/com/devos/docscanner/MainActivity.kt
+++ android-document-scanner/app/src/main/java/com/devos/docscanner/MainActivity.kt
@@ -7,6 +7,7 @@ import android.graphics.BitmapFactory
 import android.graphics.PointF
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.camera.core.CameraSelector
